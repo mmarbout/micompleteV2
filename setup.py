bc5f6dd0b509fe9848f5158ceb42e489cf2a7f47
mmarbout <57181881+mmarbout@users.noreply.github.com>
2024-10-17 10:11:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('README.rst', mode='r') as f:
     l_description = f.read()
 
 setup(name='micomplete',
-        version='1.1.1',
+        version='2.1.1',
         description='Quality control of assembled genomes',
         long_description=l_description,
-        url='https://bitbucket.org/evolegiolab/micomplete',
-        author='Eric Hugoson',
-        author_email='eric@hugoson.org',
+        url='https://github.com/mmarbout/micompleteV2/',
+        author='Martial Marbouty',
+        author_email='mmarbout@pasteur.fr',
         license='GNU General Public License v3 or later (G  PLv3+)',
         
         classifiers=[
@@ -25,7 +25,7 @@ setup(name='micomplete',
         ],
         keywords='bioinformatics completeness metagenomics',
         install_requires=[
-            'biopython>=1.53',
+            'biopython>=1.80',
             'numpy>=1.13',
             'matplotlib>=2.0.2',
             ],
