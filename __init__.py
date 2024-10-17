0756fba6468d06070b83a45e743f29a9050051e2
mmarbout <57181881+mmarbout@users.noreply.github.com>
2024-10-17 11:27:44 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Eric Hugoson.
-# See LICENSE for details.
-
-__version__ = '1.1.1'
-__all__ = ['completeness', 'linkageanalysis', 'parseseqs', 'micomplete']
-from .completeness import calcCompleteness
-from .linkageanalysis import linkageAnalysis
-from .parseseqs import parseSeqStats
